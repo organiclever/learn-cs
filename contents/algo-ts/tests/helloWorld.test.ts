6ae8edb7b6ad567f0c9128d9aaf5661f6f2f73ed
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-16 21:18:34 +0700

    test pre-commit hooks

--- contents/algo-ts/tests/helloWorld.test.ts
+++ contents/algo-ts/tests/helloWorld.test.ts
@@ -1,6 +1,5 @@
 import helloWorld from '../src/helloWorld';
 
-// TODO: Implement tests for helloWorld function
 describe('helloWorld function', () => {
   it('should return "Hello, World!"', () => {
     expect(helloWorld()).toBe('Hello, World!');

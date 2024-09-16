63afb20d7ba4fe347afac04f40ed94f2cbaf2841
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-16 22:59:53 +0700

    add format for java & xml

--- contents/algo-java/src/main/java/com/example/algorithms/HelloWorld.java
+++ contents/algo-java/src/main/java/com/example/algorithms/HelloWorld.java
@@ -1,6 +1,5 @@
 package com.example.algorithms;
 
-// test comment for formatting
 public class HelloWorld {
 
   public static String helloWorld() {
--- contents/algo-java/src/test/java/com/example/algorithms/HelloWorldTest.java
+++ contents/algo-java/src/test/java/com/example/algorithms/HelloWorldTest.java
@@ -1,9 +1,11 @@
 package com.example.algorithms;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 public class HelloWorldTest {
+
   @Test
   public void testHelloWorld() {
     assertEquals("Hello, World!", HelloWorld.helloWorld());
